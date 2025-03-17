74b7db4b7819acc643f6f170a3ffb2226cb318ff
nuryyevva <nuryyewaaynur03@gmail.com>
2025-03-17 16:07:28 +0300

    style: Put dots in the end of comments

--- tests/utils/test_shared_embedding.py
+++ tests/utils/test_shared_embedding.py
@@ -13,27 +13,29 @@ class TestSharedEmbedding:
         d_model = 5
         padding_idx = 0
 
-        # Create an instance of SharedEmbedding
+        # Create an instance of SharedEmbedding.
         shared_embedding = SharedEmbedding(vocab_size, d_model, padding_idx)
         assert (
             shared_embedding._SharedEmbedding__embedding.num_embeddings == vocab_size
-        ), "Wrong parameters of nn.Embedding"
-        assert shared_embedding._SharedEmbedding__embedding.embedding_dim == d_model, "Wrong parameters of nn.Embedding"
+        ), "Wrong parameters of nn.Embedding."
+        assert (
+            shared_embedding._SharedEmbedding__embedding.embedding_dim == d_model
+        ), "Wrong parameters of nn.Embedding."
 
-        # Create an input tensor
+        # Create an input tensor.
         input_tensor = torch.tensor([1, 2, 3, 4, 5])
 
-        # Transform the input tensor
+        # Transform the input tensor.
         output_tensor = shared_embedding(input_tensor)
 
-        # Check the shape of the output tensor
+        # Check the shape of the output tensor.
         assert output_tensor.shape == (5, d_model), "Incorrect shape of the output tensor."
 
-        # Check that the padding tokens are zeros (almost zeros due to machine precision)
+        # Check that the padding tokens are zeros (almost zeros due to machine precision).
         padding_embedding = shared_embedding(torch.tensor([padding_idx]))
         assert torch.allclose(padding_embedding, torch.zeros(1, d_model)), "Padding embedding should be zero."
 
-        # Check that the transformation is the same for the same token
+        # Check that the transformation is the same for the same token.
         token_index = 1
         token1_embedding = shared_embedding(torch.tensor([token_index]))
         token2_embedding = shared_embedding(torch.tensor([token_index]))
@@ -95,15 +97,15 @@ class TestSharedEmbedding:
         """
         shared_embedding, vocab_size, d_model = init_shared_embedding_no_padding_idx
 
-        # Create a batched input tensor
+        # Create a batched input tensor.
         batch_size = 2
         seq_len = 3
         input_tensor = torch.randint(0, vocab_size, (batch_size, seq_len))
 
-        # Embed the batched input
+        # Embed the batched input.
         output_tensor = shared_embedding(input_tensor)
 
-        # Check the shape of the output tensor
+        # Check the shape of the output tensor.
         assert output_tensor.shape == (batch_size, seq_len, d_model), "Incorrect shape for batched output."
 
     def test_no_padding(self, init_shared_embedding_no_padding_idx):
