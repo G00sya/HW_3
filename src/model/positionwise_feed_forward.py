c55948070f288a5fd62018f5fa80b0058f09b50a
Ishchenko Igor <igoryan11060@gmail.com>
2025-04-05 13:17:25 +0300

    refactor: Updated code based on review

--- src/model/positionwise_feed_forward.py
+++ src/model/positionwise_feed_forward.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class PositionwiseFeedForward(nn.Module):
-    def __init__(self, d_model: int, d_ff: int, dropout=0.1):
+    def __init__(self, d_model: int, d_ff: int, dropout: float | int = 0.1):
         """
         Initializes the PositionwiseFeedForward module.
         This module implements a feed-forward network applied to each position separately.
@@ -14,14 +14,23 @@ class PositionwiseFeedForward(nn.Module):
         """
         super().__init__()
 
-        self.w_1 = nn.Linear(d_model, d_ff)
-        self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(dropout)
+        if not isinstance(d_model, int):
+            raise TypeError(f"Dimensionality of input and output embeddings must be an int, but got {type(d_model)}.")
+        if not isinstance(d_ff, int):
+            raise TypeError(f"Dimensionality of inner layer must be an int, but got {type(d_ff)}.")
+        if not isinstance(dropout, float) and not isinstance(dropout, int):
+            raise TypeError(f"Dropout rate must be a float or an int, but got {type(dropout)}.")
+        if not 0 <= dropout <= 1:
+            raise ValueError(f"Dropout rate must be between 0 and 1, but got {dropout}.")
 
-    def forward(self, inputs: torch.Tensor):
+        self.__w_1 = nn.Linear(d_model, d_ff)
+        self.__w_2 = nn.Linear(d_ff, d_model)
+        self.__dropout = nn.Dropout(dropout)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """
         Applies the position-wise feed-forward network to the input tensor.
         :param inputs: Input tensor of shape (batch_size, seq_len, d_model).
         :return: Output tensor of shape (batch_size, seq_len, d_model).
         """
-        return self.w_2(self.dropout(F.relu(self.w_1(inputs))))
+        return self.__w_2(self.__dropout(F.relu(self.__w_1(inputs))))
