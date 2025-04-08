93c332a8c6f368b9e54519f1440d9e91ba67e95b
Ishchenko Igor <igoryan11060@gmail.com>
2025-04-08 15:34:56 +0300

    fix: Fix private class field

--- tests/model/test_positionwise_feed_forward.py
+++ tests/model/test_positionwise_feed_forward.py
@@ -13,15 +13,15 @@ class TestPositionwiseFeedForward:
 
         ffn = PositionwiseFeedForward(d_model, d_ff, dropout)
 
-        assert isinstance(ffn._w_1, nn.Linear)
-        assert isinstance(ffn._w_2, nn.Linear)
-        assert isinstance(ffn._dropout, nn.Dropout)
+        assert isinstance(ffn._PositionwiseFeedForward__w_1, nn.Linear)
+        assert isinstance(ffn._PositionwiseFeedForward__w_2, nn.Linear)
+        assert isinstance(ffn._PositionwiseFeedForward__dropout, nn.Dropout)
 
-        assert ffn._w_1.in_features == d_model
-        assert ffn._w_1.out_features == d_ff
-        assert ffn._w_2.in_features == d_ff
-        assert ffn._w_2.out_features == d_model
-        assert ffn._dropout.p == dropout
+        assert ffn._PositionwiseFeedForward__w_1.in_features == d_model
+        assert ffn._PositionwiseFeedForward__w_1.out_features == d_ff
+        assert ffn._PositionwiseFeedForward__w_2.in_features == d_ff
+        assert ffn._PositionwiseFeedForward__w_2.out_features == d_model
+        assert ffn._PositionwiseFeedForward__dropout.p == dropout
 
     def test_forward(self, init_positionwise_feed_forward) -> None:
         ffn, inputs = init_positionwise_feed_forward
@@ -40,9 +40,9 @@ class TestPositionwiseFeedForward:
     def test_valid_initialization(self):
         """Тест успешной инициализации."""
         ffn = PositionwiseFeedForward(d_model=512, d_ff=2048, dropout=0.1)
-        assert isinstance(ffn._w_1, nn.Linear)
-        assert isinstance(ffn._w_2, nn.Linear)
-        assert isinstance(ffn._dropout, nn.Dropout)
+        assert isinstance(ffn._PositionwiseFeedForward__w_1, nn.Linear)
+        assert isinstance(ffn._PositionwiseFeedForward__w_2, nn.Linear)
+        assert isinstance(ffn._PositionwiseFeedForward__dropout, nn.Dropout)
 
     def test_invalid_d_model_type(self):
         """Тест для неверного типа d_model."""
