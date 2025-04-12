17b7a3de57e0ec8f4f01ef7615266d0b05b90663
nuryyevva <nuryyewaaynur03@gmail.com>
2025-04-12 18:31:20 +0300

    test: Add tests for NoamOpt

--- tests/utils/conftest.py
+++ tests/utils/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import torch.optim as optim
+from torch import nn
 
 from src.utils.shared_embedding import SharedEmbedding
 
@@ -9,3 +11,15 @@ def init_shared_embedding_no_padding_idx():
     d_model = 5
     shared_embedding = SharedEmbedding(vocab_size, d_model)
     return shared_embedding, vocab_size, d_model
+
+
+@pytest.fixture
+def simple_model_for_noam_opt():
+    """Fixture providing a simple model with one parameter."""
+    return nn.Linear(10, 10)
+
+
+@pytest.fixture
+def sample_optimizer(simple_model_for_noam_opt):
+    """Fixture providing an Adam optimizer with zero initial LR."""
+    return optim.Adam(simple_model_for_noam_opt.parameters(), lr=0)
