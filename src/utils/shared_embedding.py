4d6a78939c3f99b17abec512fba62fc29820c7a8
Shatunov Evgeny <shatunov868@gmail.com>
2025-04-12 17:50:55 +0300

    feat: Add interface for pretrained embedding

--- src/utils/shared_embedding.py
+++ src/utils/shared_embedding.py
@@ -8,13 +8,23 @@ class SharedEmbedding(nn.Module):
     used for transforming multiple inputs with different token IDs.
     """
 
-    def __init__(self, vocab_size: int, d_model: int, padding_idx: int | None = None):
+    def __init__(
+        self,
+        vocab_size: int,
+        d_model: int,
+        padding_idx: int | None = None,
+        pretrained_embeddings: torch.Tensor | None = None,
+    ):
         """
         Initializes SharedEmbedding.
 
         :param vocab_size: The size of the vocabulary (number of embeddings). Must be a positive integer.
         :param d_model: The dimensionality of each embedding. Must be a positive integer.
         :param padding_idx: The index of the token that should be padding. Must be a non-negative integer or None.
+        :param pretrained_embeddings: Pre-trained embeddings to initialize the embedding layer with. Must be a torch.
+                                    Tensor or None.
+                                    If provided, vocab_size and d_model will be inferred from the shape of this tensor.
+                                    If None, a new embedding layer will be initialized randomly.
 
         :raises TypeError: If `vocab_size` is not an integer.
         :raises ValueError: If `vocab_size` is not positive.
@@ -22,25 +32,37 @@ class SharedEmbedding(nn.Module):
         :raises ValueError: If `d_model` is not positive.
         :raises TypeError: If `padding_idx` is not an integer or None.
         :raises ValueError: If `padding_idx` is negative.
+        :raises TypeError: If `pretrained_embeddings` is not a torch.Tensor or None.
+        :raises ValueError: If `pretrained_embeddings` is provided
+                                and its shape is incompatible with vocab_size and d_model.
         """
         super().__init__()
 
-        if not isinstance(vocab_size, int):
-            raise TypeError(f"vocab_size must be an int, but got {type(vocab_size)}")
-        if vocab_size <= 0:
-            raise ValueError(f"vocab_size must be a positive number, but got {vocab_size}")
+        if pretrained_embeddings is not None:
+            if not isinstance(pretrained_embeddings, torch.Tensor):
+                raise TypeError(
+                    f"pretrained_embeddings must be a torch.Tensor or None, but got {type(pretrained_embeddings)}"
+                )
+            self.__embedding = nn.Embedding.from_pretrained(
+                pretrained_embeddings, freeze=False, padding_idx=padding_idx
+            )
+        else:
+            if not isinstance(vocab_size, int):
+                raise TypeError(f"vocab_size must be an int, but got {type(vocab_size)}")
+            if vocab_size <= 0:
+                raise ValueError(f"vocab_size must be a positive number, but got {vocab_size}")
 
-        if not isinstance(d_model, int):
-            raise TypeError(f"d_model must be an int, but got {type(d_model)}")
-        if d_model <= 0:
-            raise ValueError(f"d_model must be a positive number, but got {d_model}")
+            if not isinstance(d_model, int):
+                raise TypeError(f"d_model must be an int, but got {type(d_model)}")
+            if d_model <= 0:
+                raise ValueError(f"d_model must be a positive number, but got {d_model}")
 
-        if padding_idx is not None and not isinstance(padding_idx, int):
-            raise TypeError(f"padding_idx must be an int or None, but got {type(padding_idx)}")
-        if padding_idx is not None and padding_idx < 0:
-            raise ValueError(f"padding_idx must be a non-negative number or None, but got {padding_idx}")
+            if padding_idx is not None and not isinstance(padding_idx, int):
+                raise TypeError(f"padding_idx must be an int or None, but got {type(padding_idx)}")
+            if padding_idx is not None and padding_idx < 0:
+                raise ValueError(f"padding_idx must be a non-negative number or None, but got {padding_idx}")
 
-        self.__embedding = nn.Embedding(vocab_size, d_model, padding_idx=padding_idx)
+            self.__embedding = nn.Embedding(vocab_size, d_model, padding_idx=padding_idx)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
