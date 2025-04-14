602f5a77d0b54f043e1eae0b88d4327057deb1b2
Ishchenko Igor <igoryan11060@gmail.com>
2025-04-14 18:34:04 +0300

    feat: Add mask

---
+++ src/model/mask.py
@@ -0,0 +1,44 @@
+import torch
+
+
+def subsequent_mask(size):
+    """
+    Creates a triangular mask to prevent attention to future positions.
+
+    This function generates a mask that is used during the decoding phase of
+    sequence-to-sequence models to prevent the model from attending to
+    future tokens in the target sequence.
+
+    :param size: The size (length) of the target sequence.
+    :return: A tensor of shape (1, size, size) with True in the valid positions and False otherwise.
+    """
+    if torch.cuda.is_available():
+        DEVICE = torch.device("cuda")
+    else:
+        DEVICE = torch.device("cpu")
+
+    mask = torch.ones(size, size, dtype=torch.bool, device=DEVICE).triu_(diagonal=1)
+    return mask.unsqueeze(0) == 0
+
+
+def make_mask(source_inputs, target_inputs, pad_idx):
+    """
+    Creates masks for source and target sequences.
+
+    This function generates two masks:
+    1.  `source_mask`:  Prevents attention to padding tokens in the source sequence.
+    2.  `target_mask`: Prevents attention to padding tokens and future tokens in the target sequence.
+
+    :param source_inputs: Source input sequence. Shape: (batch_size, source_seq_len).
+    :param target_inputs: Target input sequence. Shape: (batch_size, target_seq_len).
+    :param pad_idx: The index of the padding token in the vocabulary.
+    :return: A tuple containing:
+            - source_mask: A boolean tensor of shape (batch_size, 1, source_seq_len) indicating
+            which source tokens are not padding.
+            - target_mask: A boolean tensor of shape (batch_size, target_seq_len, target_seq_len)
+            indicating which target tokens are valid (not padding and not future tokens).
+    """
+    source_mask = (source_inputs != pad_idx).unsqueeze(-2)
+    target_mask = (target_inputs != pad_idx).unsqueeze(-2)
+    target_mask = target_mask & subsequent_mask(target_inputs.size(-1)).type_as(target_mask)
+    return source_mask, target_mask
