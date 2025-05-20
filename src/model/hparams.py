1dee4df18be03f94ccaee66753636f3c1e0ee3f6
Shatunov Evgeny <shatunov868@gmail.com>
2025-05-20 14:04:59 +0300

    chore: Create hparams file in model folder

---
+++ src/model/hparams.py
@@ -0,0 +1,11 @@
+config = dict(
+    d_ff=1024,
+    blocks_count=4,  # in Encoder and Decoder
+    heads_count=10,  # Because we use d_model=300 in navec, and it must be divisible by heads_count
+    dropout_rate=0.1,
+    epochs=30,
+    train_batch_size=16,
+    test_batch_size=32,
+    data_split_ratio=0.85,
+    use_pretrained_embedding=True,
+)
